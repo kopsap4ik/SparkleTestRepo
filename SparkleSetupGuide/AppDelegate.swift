d159aead3b1cd7adec97dc4fc226c37f21e1eea4
Vasily Petuhov <vspetukhov@icloud.com>
2021-06-08 16:45:09 +0300

    init commit

---
+++ SparkleSetupGuide/AppDelegate.swift
@@ -0,0 +1,55 @@
+//
+//  AppDelegate.swift
+//  SparkleSetupGuide
+//
+//  Created by Prashant Nigam on 10/6/19.
+//  Copyright © 2019 Prashant Nigam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    let popover = NSPopover()
+    var eventMonitor : EventMonitor?
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: NSImage.Name("Star"))
+            button.action = #selector(togglePopOver)
+        }
+        
+        popover.contentViewController = VersionDisplayViewController.freshController()
+        
+        eventMonitor = EventMonitor(mask: [.leftMouseDown, .rightMouseDown]) { [weak self] event in
+            if let strongSelf = self, strongSelf.popover.isShown {
+                strongSelf.closePopover(sender: event)
+            }
+        }
+    }
+    
+    @objc func togglePopOver(sender: Any?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+    
+    func showPopover(sender: Any?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            eventMonitor?.start()
+        }
+    }
+    
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
+        eventMonitor?.stop()
+    }
+    
+}
+
